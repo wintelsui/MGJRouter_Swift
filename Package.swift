e228835791b50c5f07ae7ef0b02babdd716f0c6e
wintelsui <wintelsui@hotmail.com>
2025-02-06 10:09:05 +0800

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version:5.0
+//
+//  Package.swift
+//
+
+import PackageDescription
+
+let package = Package(name: "MGJRouter_Swift",
+                      platforms: [.iOS(.v8)],
+                      products: [.library(name: "MGJRouter_Swift",
+                                          targets: ["MGJRouter_Swift"])],
+                      targets: [.target(name: "MGJRouter_Swift",
+                                        path: "MGJRouter_Swift",
+                                        publicHeadersPath: "")])
